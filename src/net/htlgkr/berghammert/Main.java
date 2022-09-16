182bf641de78d708099af0c1ac9d4debeb387d2d
DaBergg <BerghammerT190006@sus.htl-grieskirchen.at>
2022-09-16 11:29:24 +0200

    Initial commit

---
+++ src/net/htlgkr/berghammert/Main.java
@@ -0,0 +1,18 @@
+package net.htlgkr.berghammert;
+
+public class Main implements PrimeSieve{
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public boolean isPrime(int p) {
+        return false;
+    }
+
+    @Override
+    public void printPrimes() {
+
+    }
+}
---
+++ src/net/htlgkr/berghammert/PrimeSieve.java
@@ -0,0 +1,6 @@
+package net.htlgkr.berghammert;
+
+public interface PrimeSieve {
+    public boolean isPrime(int p);
+    public void printPrimes();
+}
