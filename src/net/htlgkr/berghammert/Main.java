fd30317d752ea4d46619b143343cb043c977c653
DaBergg <BerghammerT190006@sus.htl-grieskirchen.at>
2022-09-20 08:22:38 +0200

    Methode 'printPrimes' fertig ausprogrammiert

--- src/net/htlgkr/berghammert/Main.java
+++ src/net/htlgkr/berghammert/Main.java
@@ -2,6 +2,8 @@ package net.htlgkr.berghammert;
 
 public class Main implements PrimeSieve{
 
+    public static int obergrenze;
+
     public static void main(String[] args) {
 
     }
@@ -22,6 +24,11 @@ public class Main implements PrimeSieve{
 
     @Override
     public void printPrimes() {
-
+        for (int i = 0; i < obergrenze; i++) {
+            boolean p = isPrime(i);
+            if(p == true) {
+                System.out.println(i);
+            }
+        }
     }
 }
